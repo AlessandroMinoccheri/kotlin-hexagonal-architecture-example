85ae9fe5bf8b57074ebad38ec2e791168d656f39
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2021-04-17 20:50:45 +0200

    cosmetics

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
---
+++ src/main/kotlin/com/example/demo/payment/domain/repository/CustomerRepository.kt
@@ -0,0 +1,8 @@
+package com.example.demo.payment.domain.repository
+
+import com.example.demo.payment.domain.model.Customer
+
+interface CustomerRepository {
+    fun findAllCustomers(): List<Customer>
+    fun save(customer: Customer): Customer
+}
